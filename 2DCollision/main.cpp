74b090c5ec1e1ad68569caf8f5a8ae2503fac0c0
(Student) - David Whiteford <C00204740@itcarlow.ie>
2018-11-21 16:43:49 +0000

    Capsule Started

--- 2DCollision/main.cpp
+++ 2DCollision/main.cpp
@@ -30,22 +30,39 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	sf::Texture circle_texture;
-	if (!circle_texture.loadFromFile("assets\\circle.png")) {
-		DEBUG_MSG("Failed to load file");
-		return EXIT_FAILURE;
-	}
-	AnimatedSprite npc_circle(circle_texture);
+	//sf::Texture circle_texture;
+	//if (!circle_texture.loadFromFile("assets\\circle.png")) {
+	//	DEBUG_MSG("Failed to load file");
+	//	return EXIT_FAILURE;
+	//}
+	//AnimatedSprite npc_circle(circle_texture);
 
-	sf::Texture capsule_texture;
-	if (!capsule_texture.loadFromFile("assets\\capsule.png")) {
-		DEBUG_MSG("Failed to load file");
-		return EXIT_FAILURE;
-	}
-	AnimatedSprite npc_capsule(capsule_texture);
+	sf::CircleShape capsuleCircleR;
+	capsuleCircleR.setFillColor(sf::Color::Green);
+	capsuleCircleR.setRadius(50);
+	capsuleCircleR.setPosition(200 , 50);
+	capsuleCircleR.setOrigin(50, 50);
+
+
+	sf::CircleShape capsuleCircleL;
+	capsuleCircleL.setFillColor(sf::Color::Green);
+	capsuleCircleL.setRadius(50);
+	capsuleCircleL.setPosition(250, 50);
+	capsuleCircleL.setOrigin(50, 50);
+
+	sf::RectangleShape capsuleRect;
+	capsuleRect.setFillColor(sf::Color::Green);
+	capsuleRect.setSize(sf::Vector2f(50,50));
+	capsuleRect.setPosition(200, 50);
+	capsuleRect.setOrigin(50, 50);
+
 	
-
-
+	sf::CircleShape npc_circle;
+	npc_circle.setFillColor(sf::Color::Red);
+	npc_circle.setRadius(50);
+	npc_circle.setPosition(100, 0);
+	npc_circle.setOrigin(50, 50);
+	
 	// Setup NPC's Default Animated Sprite
 	AnimatedSprite npc_animated_sprite(npc_texture);
 	npc_animated_sprite.addFrame(sf::IntRect(3, 3, 84, 84));
@@ -68,8 +85,8 @@ int main()
 		// Setup the NPC
 		GameObject &npc = NPC(npc_animated_sprite);
 	
-		// Setup the NPC
-		/*GameObject &cicleNpc = NPC(npc_circle);*/
+		
+		
 	
 	
 	
@@ -77,6 +94,7 @@ int main()
 	GameObject &player = Player(player_animated_sprite);
 	//Setup NPC AABB
 	c2AABB aabb_npc;
+
 	aabb_npc.min = c2V(npc.getAnimatedSprite().getPosition().x, npc.getAnimatedSprite().getPosition().y);
 	aabb_npc.max = c2V(
 		npc.getAnimatedSprite().getPosition().x +
@@ -84,7 +102,20 @@ int main()
 		npc.getAnimatedSprite().getPosition().y +
 		npc.getAnimatedSprite().getGlobalBounds().height);
 
-	c2Circle
+	c2Circle circle_npc;
+
+	/*circle_npc.x = npc_circle.getGlobalBounds().x;*/
+	circle_npc.p = c2V(npc_circle.getPosition().x,
+		npc_circle.getPosition().y);
+	circle_npc.r = npc_circle.getRadius();
+
+	c2Capsule capsule_npc;
+	capsule_npc.a = c2V(capsuleCircleL.getPosition().x,
+		capsuleCircleL.getPosition().y);
+	capsule_npc.b = c2V(capsuleCircleR.getPosition().x,
+		capsuleCircleR.getPosition().y);
+
+	
 	//Setup Player AABB
 	c2AABB aabb_player;
 	aabb_player.min = c2V(player.getAnimatedSprite().getPosition().x, player.getAnimatedSprite().getPosition().y);
@@ -96,6 +127,7 @@ int main()
 
 	// Collision result
 	int result = 0;
+	int circletoAABBresult = 0;
 
 	// Direction of movement of NPC
 	sf::Vector2f direction(0.1f, 0.2f);
@@ -107,30 +139,27 @@ int main()
 		player.getAnimatedSprite().setPosition(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
 		
 		// Move The NPC
-		sf::Vector2f move_to(npc.getAnimatedSprite().getPosition().x + direction.x, npc.getAnimatedSprite().getPosition().y + direction.y);
+		sf::Vector2f move_to(npc.getAnimatedSprite().getPosition().x + direction.x, npc.getAnimatedSprite().getPosition().y + direction.y );
 		sf::Vector2f move(npc_circle.getPosition().x + direction.x, npc_circle.getPosition().y + direction.y);
-		sf::Vector2f capsule_move(npc_capsule.getPosition().x + direction.x, npc_capsule.getPosition().y + direction.y);
+		/*sf::Vector2f capsule_move(npc_capsule.getPosition().x + direction.x, npc_capsule.getPosition().y + direction.y);*/
 
-		if (move_to.x < 0 && move.x < 0 && capsule_move.x < 0)
+		if (move_to.x < 0)
 		{
 			direction.x *= -1;
 			move_to.x = 0 + npc.getAnimatedSprite().getGlobalBounds().width;
 		}
-		else if (move_to.x + npc.getAnimatedSprite().getGlobalBounds().width >= 800 
-			&& move.x + npc_circle.getGlobalBounds().width >= 800
-			&& capsule_move.x + npc_capsule.getGlobalBounds().width >= 800)
+		else if (move_to.x + npc.getAnimatedSprite().getGlobalBounds().width >= 800)
 		{ 
 			direction.x *= -1;
 			move_to.x = 800 - npc.getAnimatedSprite().getGlobalBounds().width;
 		}
-		else if (move_to.y < 0 && move.y < 0 && capsule_move.y < 0)
+		else if (move_to.y < 0 )
 		{ 
 			direction.y *= -1;
 			move_to.y = 0 + npc.getAnimatedSprite().getGlobalBounds().height;
 		}
 		else if (move_to.y + npc.getAnimatedSprite().getGlobalBounds().height >= 600 
-			&& move.y + npc_circle.getGlobalBounds().height >= 600
-			&& capsule_move.y + npc_capsule.getGlobalBounds().height >= 600)
+		)
 		{
 			direction.y *= -1;
 			move_to.y = 600 - npc.getAnimatedSprite().getGlobalBounds().height;
@@ -143,12 +172,14 @@ int main()
 		npc.getAnimatedSprite().setPosition(move_to);
 
 		// Update NPC AABB set x and y
-		aabb_npc.min = c2V(
+		aabb_npc.min = c2V
+		(
 			npc.getAnimatedSprite().getPosition().x, 
 			npc.getAnimatedSprite().getPosition().y
 		);
 
-		aabb_npc.max = c2V(
+		aabb_npc.max = c2V
+		(
 			npc.getAnimatedSprite().getPosition().x +
 			npc.getAnimatedSprite().getGlobalBounds().width,
 			npc.getAnimatedSprite().getPosition().y +
@@ -156,16 +187,23 @@ int main()
 		);
 
 		// Update Player AABB
-		aabb_player.min = c2V(
+		aabb_player.min = c2V
+		(
 			player.getAnimatedSprite().getPosition().x, 
 			player.getAnimatedSprite().getPosition().y
 		);
-		aabb_player.max = c2V(
+		aabb_player.max = c2V
+		(
 			player.getAnimatedSprite().getPosition().x +
 			player.getAnimatedSprite().getGlobalBounds().width, 
 			player.getAnimatedSprite().getPosition().y + 
 			player.getAnimatedSprite().getGlobalBounds().height
 		);
+		//updated npc circle
+		circle_npc.p = c2V(npc_circle.getPosition().x ,
+			npc_circle.getPosition().y);
+		circle_npc.r = npc_circle.getRadius();
+
 
 		// Process events
 		sf::Event event;
@@ -208,10 +246,14 @@ int main()
 
 		// Check for collisions
 		result = c2AABBtoAABB(aabb_player, aabb_npc);
+		circletoAABBresult = c2CircletoAABB(circle_npc, aabb_player);
 		cout << ((result != 0) ? ("Collision") : "") << endl;
-		if (result)
+		cout << ((circletoAABBresult != 0) ? ("Collision") : "") << endl;
+		
+		
+		if (result || circletoAABBresult)
 		{
-			player.getAnimatedSprite().setColor(sf::Color(255,0,0));
+			player.getAnimatedSprite().setColor(sf::Color::Magenta);
 		}
 		else 
 		{
@@ -227,7 +269,9 @@ int main()
 		// Draw the NPC's Current Animated Sprite
 		window.draw(npc.getAnimatedSprite());
 		window.draw(npc_circle);
-		window.draw(npc_capsule);
+		window.draw(capsuleCircleL);
+		window.draw(capsuleCircleR);
+		window.draw(capsuleRect);
 
 		// Update the window
 		window.display();
